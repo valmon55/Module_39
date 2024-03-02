0cfd4aa8feaa314e3ea6bbdfb7154de4176268e7
valmon55 <fedor_ka@mail.ru>
2024-03-02 18:11:23 +0300

    + валидация емейл с помощью тригера

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new LoginPage(); //RegisterPage();                        
+            MainPage = new RegisterPage();                        
         }
 
         protected override void OnStart()
