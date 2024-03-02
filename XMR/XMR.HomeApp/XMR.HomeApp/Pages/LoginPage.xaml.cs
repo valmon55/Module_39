1c01df15e068d97352d995b21a6df162a7109da7
valmon55 <fedor_ka@mail.ru>
2024-03-02 13:49:26 +0300

    из модуля 38

---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public const string BUTTON_TEXT = "Войти";
+        public static int loginCounter = 0;
+        public LoginPage()
+        {
+            InitializeComponent();
+            if(Device.RuntimePlatform == Device.UWP)
+            {
+                loginButton.CornerRadius = 0;
+            }
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if (loginCounter == 0)
+            {
+                // Если первая попытка - просто меняем сообщения
+                loginButton.Text = $"Выполняется вход..";
+            }
+            else if (loginCounter > 5) // Слишком много попыток - показываем ошибку
+            {
+                // Деактивируем кнопку
+                loginButton.IsEnabled = false;
+                // Показываем текстовое сообщение об ошибке
+                var infoMessage = (Label)stackLayout.Children.Last();
+                infoMessage.Text = "Слишком много попыток! Попробуйте позже.";
+                //infoMessage.TextColor= Color.FromRgb(255,0,0);
+                //stackLayout.Children.Add(new Label
+                //{
+                //    Text = "Слишком много попыток! Попробуйте позже.",
+                //    TextColor = Color.Red,
+                //    VerticalTextAlignment = TextAlignment.Center,
+                //    HorizontalTextAlignment = TextAlignment.Center,
+                //    Padding = new Thickness()
+                //    {
+                //        Bottom = 30,
+                //        Left = 10,
+                //        Top = 30,
+                //        Right = 10
+                //    }
+                //});                
+            }
+            else
+            {
+                // Изменяем текст кнопки и показываем количество попыток входа
+                loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCounter}";
+            }
+
+            // Увеличиваем счетчик
+            loginCounter += 1;
+        }
+    }
+}
---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/NewDevicePage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewDevicePage : ContentPage
+    {
+        public NewDevicePage()
+        {
+            InitializeComponent();
+            OpenEditor();
+        }
+        public void OpenEditor()
+        {
+            // Создание однострочного текстового поля для названия
+            var newDeviceName = new Entry
+            {
+                BackgroundColor = Color.AliceBlue,
+                Margin = new Thickness(30, 10),
+                Placeholder = "Название"
+            };
+            // Создание многострочного поля для описания
+            var newDeviceDescription = new Editor
+            {
+                HeightRequest = 200,
+                BackgroundColor = Color.AliceBlue,
+                Margin = new Thickness(30, 10),
+                Placeholder = "Описание"
+            };
+
+            // Создаем заголовок для переключателя
+            var switchHeader = new Label { Text = "Не использует газ", HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(0, 5, 0, 0) };
+            stackLayout.Children.Add(switchHeader);
+
+            // Создаем переключатель
+            Switch switchControl = new Switch
+            {
+                IsToggled = false,
+                HorizontalOptions = LayoutOptions.Center,
+                ThumbColor = Color.DodgerBlue,
+                OnColor = Color.LightSteelBlue,
+            };
+
+            // Регистрируем обработчик события переключения
+            switchControl.Toggled += (sender, e) => SwitchHandler(sender, e, switchHeader);
+
+            // Создание кнопки
+            var addButton = new Button
+            {
+                Text = "Добавить",
+                Margin = new Thickness(30, 10),
+                BackgroundColor = Color.Silver,
+            };
+
+            // Добавляем всё на страницу
+            stackLayout.Children.Add(newDeviceName);
+            stackLayout.Children.Add(newDeviceDescription);
+            stackLayout.Children.Add(switchControl);
+            stackLayout.Children.Add(addButton);
+        }
+        /// <summary>
+        /// Обработка переключателя
+        /// </summary>
+        public void SwitchHandler(object sender, ToggledEventArgs e, Label header)
+        {
+            if (!e.Value)
+            {
+                header.Text = "Не использует газ";
+                return;
+            }
+
+            header.Text = "Использует газ";
+        }
+    }
+}
